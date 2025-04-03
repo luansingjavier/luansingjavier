6587bb76040f1def9d2285b59317b4b238096133
Javier <luansingjavier@gmail.com>
2025-04-03 20:16:39 +0800

    chore: add search engine optimization

--- next.config.js
+++ next.config.js
@@ -4,30 +4,11 @@ const nextConfig = {
   i18n: {
     locales: ["default", "en-US", "en-GB", "es", "fr", "de"],
     defaultLocale: "default",
+    localeDetection: true,
     domains: [
       {
         domain: "luansingjavier.info",
         defaultLocale: "default",
-      },
-      {
-        domain: "luansingjavier.info/en-us",
-        defaultLocale: "en-US",
-      },
-      {
-        domain: "luansingjavier.info/en-gb",
-        defaultLocale: "en-GB",
-      },
-      {
-        domain: "luansingjavier.info/es",
-        defaultLocale: "es",
-      },
-      {
-        domain: "luansingjavier.info/fr",
-        defaultLocale: "fr",
-      },
-      {
-        domain: "luansingjavier.info/de",
-        defaultLocale: "de",
       },
     ],
   },
