8a46abdef3c9011f64ffa3b7368f3507e54b8d52
Javier <luansingjavier@gmail.com>
2025-04-03 19:51:33 +0800

    chore: add search engine optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["default", "en-US", "en-GB", "es", "fr", "de"],
+    defaultLocale: "default",
+    domains: [
+      {
+        domain: "luansingjavier.info",
+        defaultLocale: "default",
+      },
+      {
+        domain: "luansingjavier.info/en-us",
+        defaultLocale: "en-US",
+      },
+      {
+        domain: "luansingjavier.info/en-gb",
+        defaultLocale: "en-GB",
+      },
+      {
+        domain: "luansingjavier.info/es",
+        defaultLocale: "es",
+      },
+      {
+        domain: "luansingjavier.info/fr",
+        defaultLocale: "fr",
+      },
+      {
+        domain: "luansingjavier.info/de",
+        defaultLocale: "de",
+      },
+    ],
+  },
+  images: {
+    domains: ["luansingjavier.info"],
+  },
 };
 
 module.exports = nextConfig;
